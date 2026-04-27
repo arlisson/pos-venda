a7f32aaf1240ead836bb4e9ee95a17a2dfef2995
arlisson <arlisson234529@gmail.com>
2026-04-27 15:40:01 -0300

    links internos

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -14,8 +14,12 @@ export const RETURN_STAGES = [
 export const OPERATORS = ['Vivo', 'TIM', 'Claro'];
 
 export const EXTERNAL_LINKS = [
-  { id: 'gov', name: 'Receita Federal', url: 'https://www.gov.br/receitafederal/', dot: 'gov' },
-  { id: 'vivo', name: 'Vivo Empresas', url: 'https://www.vivo.com.br/empresas/', dot: 'vivo' },
-  { id: 'tim', name: 'TIM Empresas', url: 'https://www.tim.com.br/empresas/', dot: 'tim' },
-  { id: 'claro', name: 'Claro Empresas', url: 'https://www.claro.com.br/empresas/', dot: 'claro' },
+  { id: 'receita-cnpj', name: 'Receita CNPJ', url: 'https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/Cnpjreva_Solicitacao.asp?cnpj=65186256000180', dot: 'gov' },
+  { id: 'abr-telecom', name: 'ABR Telecom', url: 'https://consultanumero.abrtelecom.com.br/consultanumero/consulta/consultaSituacaoAtualCtg', dot: 'abr' },
+  { id: 'vivo-mve', name: 'Vivo MVE', url: 'https://mve.vivo.com.br/oauth?logout=true', dot: 'vivo' },
+  { id: 'vivo-cobertura', name: 'Vivo Cobertura', url: 'https://vivo.com.br/para-voce/por-que-vivo/qualidade/cobertura', dot: 'vivo' },
+  { id: 'tim-negocia', name: 'TIM Negocia', url: 'https://timnegocia.com.br/', dot: 'tim' },
+  { id: 'meu-tim', name: 'Meu TIM', url: 'https://meutim.tim.com.br/novo', dot: 'tim' },
+  { id: 'claro-acesso', name: 'Minha Claro', url: 'https://minhaclaro.claro.com.br/acesso-rapido/', dot: 'claro' },
+  { id: 'claro-cobertura', name: 'Claro Cobertura', url: 'https://www.claro.com.br/mapa-de-cobertura?srsltid=AfmBOoo6nyH43Cy_Tmy1RbxKaKLlpmftwCGEshrnN4FZz__aZVj2NppU', dot: 'claro' },
 ];
